32ea339b3e1e1ee2b39ba7877ecc11ac8cdc7fa2
Peter1811 <shandryuk.petr@yandex.ru>
2022-07-05 11:44:40 +0300

    Create model
    Create model Films

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+    class Meta:
+        verbose_name = "Фильм"
+        verbose_name_plural = "Фильмы"
+
+    name = models.CharField(max_length=200)
+    text = models.TextField()
+    screensaver_reference = models.URLField()
+    magnet_reference = models.URLField()
