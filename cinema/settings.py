6c449f7b6235d1899a303a2e33df408de9d61031
opium_slatt <62149273+vampsh00ta@users.noreply.github.com>
2022-07-05 17:55:16 +0300

    Update settings.py

--- cinema/settings.py
+++ cinema/settings.py
@@ -114,8 +114,8 @@ AUTH_USER_MODEL = 'main.Customer'
 #Email
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_HOST_USER = 'trofimov.vlad-1234@yandex.ru'
-EMAIL_HOST_PASSWORD = '071002071002'
+EMAIL_HOST_USER = '******'
+EMAIL_HOST_PASSWORD = '*****'
 EMAIL_PORT = 587
 
 # Static files (CSS, JavaScript, Images)
