d417454b649f3aee44fb12417f40c75732801e69
V0lbD <vova_zl@mail.ru>
2022-07-08 13:15:56 +0300

    update templates

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from main import views
+from . import views
 
 urlpatterns = [
-    path("", views.main_page)
-
+    path("", views.main_page, name='name_page'),
+    path('account/', views.account, name='account'),
+    path('filmpage/', views.filmpage, name='filmpage'),
+    path('list-of-films/', views.list_of_films, name='list_of_films'),
+    path('recovery-new-password/', views.recovery_new_password, name='recovery_new_password'),
+    path('change-new-password/', views.change_new_password, name='change_new_password'),
+    path('sign-up-page/', views.sign_up_page, name='sign_up_page'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,29 @@ from django.shortcuts import render
 
 def main_page(request):
     return render(request, 'main/main_page.html')
+
+
+def account(request):
+    return render(request, 'main/account.html')
+
+
+def filmpage(request):
+    return render(request, 'main/filmpage.html')
+
+
+def list_of_films(request):
+    return render(request, "main/List of films from user's account.html")
+
+
+def recovery_new_password(request):
+    return render(request, "main/recovery-new-password.html")
+
+
+def change_new_password(request):
+    return render(request, "main/change-new-password.html")
+
+
+def sign_up_page(request):
+    return render(request, "main/sign-up-page.html")
+
+
