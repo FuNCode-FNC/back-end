84f5580fbaaac93b05a68d7f4464bb14fc5bc380
Yeloki <gtgtr3000@gmail.com>
2022-07-10 21:17:02 +0300

    ready to deploy

--- cinema/settings.py
+++ cinema/settings.py
@@ -80,10 +80,22 @@ WSGI_APPLICATION = 'cinema.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "d1kd0dg941mcu1",
+        'USER': "dprkgmhaelhnah",
+        'PASSWORD': "80028ca9aa826b9e8a2939e53acaa702d369a0e1db71a5cce9bf0955625b075f",
+        'HOST': "ec2-23-23-151-191.compute-1.amazonaws.com",
+        'PORT': "5432",
+
     }
 }
 
